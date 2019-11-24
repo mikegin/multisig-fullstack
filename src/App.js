59e376732d0a7b5195aac1dbf370c5ab79395be9
Mike Gindin <gindin.mike@gmail.com>
2019-11-24 15:18:43 -0500

    minor refactoring

--- src/App.js
+++ src/App.js
@@ -1,121 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import Web3 from 'web3';
-import { Accounts } from 'web3-eth-accounts';
-
+import { CONTRACT_ADDRESS, CONTRACT_ABI } from './contract/constants.js';
 
 class App extends React.Component {
   state = {
     contract: null,
-    account: null
+    accounts: null,
+    destination: null,
   }
 
   async componentDidMount() {
     const web3 = new Web3(window.ethereum);
     await window.ethereum.enable();
-    web3.eth.getAccounts((e, r) => { console.log(r); this.setState({ account: r}) })
-    const contract = new web3.eth.Contract([
-      {
-        "constant": false,
-        "inputs": [],
-        "name": "allowSending",
-        "outputs": [],
-        "payable": false,
-        "stateMutability": "nonpayable",
-        "type": "function"
-      },
-      {
-        "constant": false,
-        "inputs": [
-          {
-            "internalType": "address payable",
-            "name": "destination",
-            "type": "address"
-          },
-          {
-            "internalType": "uint256",
-            "name": "value",
-            "type": "uint256"
-          }
-        ],
-        "name": "send",
-        "outputs": [],
-        "payable": false,
-        "stateMutability": "nonpayable",
-        "type": "function"
-      },
-      {
-        "payable": true,
-        "stateMutability": "payable",
-        "type": "fallback"
-      },
-      {
-        "inputs": [
-          {
-            "internalType": "address",
-            "name": "_one",
-            "type": "address"
-          },
-          {
-            "internalType": "address",
-            "name": "_two",
-            "type": "address"
-          }
-        ],
-        "payable": false,
-        "stateMutability": "nonpayable",
-        "type": "constructor"
-      },
-      {
-        "constant": true,
-        "inputs": [],
-        "name": "getBalance",
-        "outputs": [
-          {
-            "internalType": "uint256",
-            "name": "",
-            "type": "uint256"
-          }
-        ],
-        "payable": false,
-        "stateMutability": "view",
-        "type": "function"
-      }
-    ], '0xf1d74C2C7f3f2767675Cd5dd66497073961DBF13');
-    // NameContract.methods.getBalance().call((err, res) => console.log(res));
-    // NameContract.methods.send('0x7Dc84745ff4E6A53805F2f46985Fa002D8d1beeD', 10).call((err, res) => {
-    //   if(err) {
-    //     console.log(err)
-    //   } else {
-    //     console.log(res)
-    //   }
-    // })
+
+    web3.eth.getAccounts((err, accounts) => this.setState({ accounts }))
+    window.ethereum.on('accountsChanged', (accounts) => {
+      this.setState({ accounts })
+    })
+
+    const contract = new web3.eth.Contract(CONTRACT_ABI, CONTRACT_ADDRESS);
     this.setState({ contract })
   }
-handleClick = async () => {
-  try {
-  await window.web3.eth.sendTransaction({ to: '0xf1d74C3C7f3f2767675Cd5dd66497073961DBF13', value: 100 })
-  } catch (e) {
-    console.log(e)
-  }
-}
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
+          <h1>Multi Sig dApp</h1>
           <button onClick={() => this.state.contract.methods.getBalance().call((err, res) => console.log(res))}>Get Balance</button>
-          <button onClick={() => this.state.contract.methods.allowSending().send({from: this.state.account[0]})}>Allow Sending</button>
-          <button onClick={() => this.state.contract.methods.send('0x7Dc84745ff4E6A53805F2f46985Fa002D8d1beeD', 10).send({from: this.state.account[0]})}>Send</button>
-          <button onClick={this.handleClick}>tranfer</button>
-
+          <button onClick={() => this.state.contract.methods.allowSending().send({from: this.state.accounts[0]})}>Allow Sending</button>
+          <div>
+            <input type="text" placeholder="destination address" onChange={e => this.setState({ destination: e.target.value })}/>
+            <button onClick={() => this.state.contract.methods.send(this.state.destination, 10).send({ from: this.state.accounts[0] })}>Send</button>
+          </div>
         </header>
       </div>
     );
   }
-  
 }
 
 export default App;
---
+++ src/contract/constants.js
@@ -0,0 +1,69 @@
+export const CONTRACT_ADDRESS = '0xf1d74c2c7f3f2767675cd5dd66497073961dbf13';
+export const CONTRACT_ABI = [
+  {
+    "constant": false,
+    "inputs": [],
+    "name": "allowSending",
+    "outputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "internalType": "address payable",
+        "name": "destination",
+        "type": "address"
+      },
+      {
+        "internalType": "uint256",
+        "name": "value",
+        "type": "uint256"
+      }
+    ],
+    "name": "send",
+    "outputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "payable": true,
+    "stateMutability": "payable",
+    "type": "fallback"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "_one",
+        "type": "address"
+      },
+      {
+        "internalType": "address",
+        "name": "_two",
+        "type": "address"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "constructor"
+  },
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "getBalance",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  }
+]
